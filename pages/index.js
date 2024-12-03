b47a669bce98cea5345a5a9ac46766df6e21c351
AdnanAmin045 <adnanamin.available@gmail.com>
2024-12-03 10:26:39 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+    <>
+        <h1>Hello, Static Site Generation!</h1>
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20230816191453/gfglogo.png" alt="page" />
+    </>
+    )
+}
